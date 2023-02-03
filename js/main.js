5083b218f85284d04b72933380ba63a0a779c6ab
Your Name <you@example.com>
2023-02-02 20:48:54 -0500

    working fine

--- js/main.js
+++ js/main.js
@@ -2,16 +2,12 @@ let page = 1;
 const perPage = 10;
 
 function loadMovieData(title = null) {
-  let url =
-    "https://famous-bull-undershirt.cyclic.app/api/movies?page=" +
-    page +
-    "&perPage=" +
-    perPage;
+  let url = `https://famous-bull-undershirt.cyclic.app/api/movies?page=${page}&perPage=${perPage}`;
 
   if (title) {
-    url += "&title=" + title;
-    page = 1;
+    url += `&title=${title}`;
     document.querySelector(".pagination").classList.add("d-none");
+    page = 1;
   } else {
     document.querySelector(".pagination").classList.remove("d-none");
   }
@@ -19,74 +15,92 @@ function loadMovieData(title = null) {
   fetch(url)
     .then((response) => response.json())
     .then((data) => {
-      // Creating the <tr> Elements
       let movieRows = data.map((movie) => {
-        let runtime =
-          Math.floor(movie.runtime / 60) +
-          ":" +
-          (movie.runtime % 60).toString().padStart(2, "0");
-        return `<tr data-id="${movie._id}">
-                        <td>${movie.year}</td>
-                        <td>${movie.title}</td>
-                        <td>${movie.plot ? movie.plot : "N/A"}</td>
-                        <td>${movie.rated ? movie.rated : "N/A"}</td>
-                        <td>${runtime}</td>
-                    </tr>`;
-      });
-      // Adding <tr> Elements to the Table
-      let tableBody = document.querySelector("#moviesTable tbody");
-      tableBody.innerHTML = movieRows.join("");
+        let plot = movie.plot || "N/A";
+        let rated = movie.rated || "N/A";
+        let runtime = movie.runtime;
+        let hours = runtime ? Math.floor(runtime / 60) : "N/A";
+        let minutes = runtime
+          ? (runtime % 60).toString().padStart(2, "0")
+          : "N/A";
 
-      movieRows.forEach((row) => {
-        let tr = document.createElement("tr");
-        tr.innerHTML = row;
-        tableBody.appendChild(tr);
+        return `
+        <tr data-id="${movie._id}">
+          <td>${movie.year}</td>
+          <td>${movie.title}</td>
+          <td>${plot}</td>
+          <td>${rated}</td>
+          <td>${hours}:${minutes}</td>
+        </tr>
+        `;
       });
 
-      // Updating the "Current Page"
-      const currentPage = document.querySelector("#current-page");
-      currentPage.innerHTML = page;
-    })
-    .catch((error) => console.error(error));
+      document.querySelector("tbody").innerHTML = movieRows.join("");
 
-  // select all of the <tr> elements in the table
-  let movieRows = document.querySelectorAll("table tr");
+      let movieRowsArray = Array.from(document.querySelectorAll("tbody tr"));
+      movieRowsArray.forEach((row) => {
+        row.addEventListener("click", (e) => {
+          let id = e.currentTarget.getAttribute("data-id");
+          fetch(`https://famous-bull-undershirt.cyclic.app/api/movies/${id}`)
+            .then((response) => response.json())
+            .then((data) => {
+              // Populate and display the modal with the movie data
+              document.querySelector(".modal-title").innerHTML = data.title;
 
-  // loop through the <tr> elements and add a click event to each one
-  movieRows.forEach((row) => {
-    row.addEventListener("click", (event) => {
-      // get the value of the "data-id" attribute of the clicked element
-      let movieId = event.target.getAttribute("data-id");
+              // build the HTML for the modal body
+              let modalBodyHTML = `<img class="img-fluid w-100" src="${data.poster}"><br><br>`;
+              modalBodyHTML += `<strong>Directed By:</strong> ${data.directors.join(
+                ", "
+              )}<br><br>`;
+              if (data.fullplot) {
+                modalBodyHTML += `<p>${data.fullplot.substring(0, 200)}...</p>`;
+              } else {
+                modalBodyHTML += `<p>N/A</p>`;
+              }
+              modalBodyHTML += `<strong>Awards:</strong> ${data.awards.text}<br>`;
+              modalBodyHTML += `<strong>IMDB Rating:</strong> ${data.imdb.rating} (votes: ${data.imdb.votes})`;
 
-      // make a request for data using the path: /api/movies/data-id
-      fetch(`https://famous-bull-undershirt.cyclic.app/api/movies/${movieId}`)
-        .then((response) => response.json())
-        .then((data) => {
-          // set the "modal-title" of your "detailsModal" to show the value of the title property
-          document.querySelector(".modal-title").innerHTML = data.title;
+              // update the modal body with the new HTML
+              document.querySelector(".modal-body").innerHTML = modalBodyHTML;
 
-          // build the HTML for the modal body
-          let modalBodyHTML = `<img class="img-fluid w-100" src="${data.poster}"><br><br>`;
-          modalBodyHTML += `<strong>Directed By:</strong> ${data.directors.join(
-            ", "
-          )}<br><br>`;
-          modalBodyHTML += `<p>${data.fullplot.substring(0, 200)}...</p>`;
-          if (data.cast) {
-            modalBodyHTML += `<strong>Cast:</strong> ${data.cast.join(
-              ", "
-            )}<br><br>`;
-          } else {
-            modalBodyHTML += `<strong>Cast:</strong> N/A<br><br>`;
-          }
-          modalBodyHTML += `<strong>Awards:</strong> ${data.awards.text}<br>`;
-          modalBodyHTML += `<strong>IMDB Rating:</strong> ${data.imdb.rating} (votes: ${data.imdb.votes})`;
-
-          // update the modal body with the new HTML
-          document.querySelector(".modal-body").innerHTML = modalBodyHTML;
-
-          // show the modal
-          $("#detailsModal").modal("show");
+              // show the modal
+              $("#detailsModal").modal("show");
+            });
         });
-    });
-  });
+      });
+
+      document.querySelector("#current-page").innerHTML = page;
+    })
+    .catch((error) => console.log(error));
 }
+
+document.addEventListener("DOMContentLoaded", function () {
+  loadMovieData();
+
+  document
+    .querySelector("#previous-page")
+    .addEventListener("click", function () {
+      if (page > 1) {
+        page--;
+        loadMovieData();
+      }
+    });
+
+  document.querySelector("#next-page").addEventListener("click", function () {
+    page++;
+    loadMovieData();
+  });
+
+  document
+    .querySelector("#searchForm")
+    .addEventListener("submit", function (e) {
+      e.preventDefault();
+      let title = document.querySelector("#title").value;
+      loadMovieData(title);
+    });
+
+  document.querySelector("#clearForm").addEventListener("click", function () {
+    document.querySelector("#title").value = "";
+    loadMovieData();
+  });
+});
